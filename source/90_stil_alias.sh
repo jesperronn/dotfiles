1c1a98efd4393291a91203f1093ab4ca8fa4f988
Jesper Rønn-Jensen <jesperrr@gmail.com>
2026-04-19 12:02:43 +0200

    refactor(stil_cert): Move to real function so that it still works without alias/function even in a subshell
    This opens for a simpler bash shell init and suppresses error message in Codex whil running sandboxed

--- source/90_stil_alias.sh
+++ source/90_stil_alias.sh
@@ -6,9 +6,3 @@ export STIL_HOME="${HOME}/src/stil"
 export KEYCLOAK_HOME="${HOME}/src/stil/binaries/rh-sso-7.6"
 export PATH="${PATH}:${KEYCLOAK_HOME}/bin"
 export PATH="${PATH}:${HOME}/src/stil/support-tools/bin"
-
-stil_cert() {
-  eval "pbpaste | normalize_cert | openssl x509 -text -noout"
-}
-export -f stil_cert
-
